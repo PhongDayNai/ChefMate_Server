99f6423f548e8b590dd8d1728266d0ee487d8261
Dương Phong <dhphong266@gmail.com>
2025-06-02 00:44:35 +0700

    update database

--- be_db_sql.sql
+++ be_db_sql.sql
@@ -12,7 +12,8 @@ CREATE TABLE Recipes(
   recipeId INT PRIMARY KEY IDENTITY(1,1),
   recipeName NVARCHAR(100) NOT NULL,
   image NVARCHAR(1000) NOT NULL,
-  likeQuantity INT DEFAULT 0
+  likeQuantity INT DEFAULT 0,
+  userId INT FOREIGN KEY REFERENCES Users(userId)
 );
 
 CREATE TABLE Ingredients(
@@ -28,18 +29,6 @@ CREATE TABLE RecipesIngredients(
   unit NVARCHAR(20) NOT NULL
 );
 
-CREATE TABLE UsersLike(
-  ulId INT PRIMARY KEY IDENTITY(1, 1),
-  userId INT FOREIGN KEY REFERENCES Users(userId),
-  recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId)
-);
-
-CREATE TABLE UsersComment(
-  ucId INT PRIMARY KEY IDENTITY(1, 1),
-  userId INT FOREIGN KEY REFERENCES Users(userId),
-  content NVARCHAR(4000) NOT NULL
-);
-
 CREATE TABLE CookingSteps(
   csId INT PRIMARY KEY IDENTITY(1, 1),
   recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId),
@@ -51,6 +40,20 @@ CREATE TABLE UsersViewRecipesHistory(
   uvrId INT PRIMARY KEY IDENTITY(1, 1),
   userId INT FOREIGN KEY REFERENCES Users(userId),
   recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId)
+);
+
+CREATE TABLE UsersLike(
+  ulId INT PRIMARY KEY IDENTITY(1, 1),
+  userId INT FOREIGN KEY REFERENCES Users(userId),
+  recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId)
+);
+
+CREATE TABLE UsersComment(
+  ucId INT PRIMARY KEY IDENTITY(1, 1),
+  userId INT FOREIGN KEY REFERENCES Users(userId),
+  recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId),
+  content NVARCHAR(4000) NOT NULL,
+  createdAt DATE DEFAULT GETDATE()
 );
 
 
@@ -129,11 +132,46 @@ GROUP BY r.recipeId, r.recipeName, r.image, r.likeQuantity
 ORDER BY viewCount DESC;
 
 -- Lấy lịch sử xem công thức
-SELECT * FROM UsersViewRecipesHistory WHERE UserId = @userId
+SELECT * FROM UsersViewRecipesHistory WHERE UserId = 1
+
+
+-- Lấy toàn bộ công thức
+SELECT 
+    r.recipeId,
+    r.recipeName,
+    r.image,
+    r.likeQuantity
+FROM Recipes r;
+
+SELECT 
+    cs.recipeId,
+    cs.indexStep,
+    cs.content AS stepContent
+FROM CookingSteps cs
+ORDER BY cs.recipeId, cs.indexStep;
+
+SELECT 
+    ri.recipeId,
+    i.ingredientId,
+    i.ingredientName,
+    ri.weight,
+    ri.unit
+FROM RecipesIngredients ri
+JOIN Ingredients i ON ri.ingredientId = i.ingredientId
+ORDER BY ri.recipeId;
 
 -- Kiểm tra thông tin bảng
+SELECT * FROM Users;
 SELECT * FROM Recipes;
 SELECT * FROM Ingredients;
 SELECT * FROM RecipesIngredients;
 SELECT * FROM CookingSteps;
 SELECT * FROM UsersViewRecipesHistory;
+
+DROP TABLE UsersViewRecipesHistory
+DROP TABLE CookingSteps
+DROP TABLE RecipesIngredients
+DROP TABLE Ingredients
+DROP TABLE UsersLike
+DROP TABLE UsersComment
+DROP TABLE Recipes
