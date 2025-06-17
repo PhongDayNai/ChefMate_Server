7e3ef1009c75010afc5edef6bd644120f21cb902
Dương Phong <dhphong266@gmail.com>
2025-06-17 09:26:13 +0700

    update recipe Apis

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -9,5 +9,6 @@ router.post('/search', recipeController.searchRecipe);
 router.post('/direct', recipeController.getDirectRecipe);
 router.get('/ingredients', recipeController.getAllIngredients);
 router.get('/top-trending', recipeController.getTopTrending);
+router.post('/increase-view-count', recipeController.increaseViewCount);
 
 module.exports = router;
