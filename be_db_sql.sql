7c3d57675d37715e132f2226cb3019cac9666378
Dương Phong <dhphong266@gmail.com>
2025-06-23 18:38:52 +0700

    update database by removing no-use table

--- be_db_sql.sql
+++ be_db_sql.sql
@@ -54,13 +54,6 @@ CREATE TABLE CookingSteps(
   content NVARCHAR(4000) NOT NULL
 );
 
-CREATE TABLE UsersViewRecipesHistory(
-  uvrId INT PRIMARY KEY IDENTITY(1, 1),
-  userId INT FOREIGN KEY REFERENCES Users(userId),
-  recipeId INT FOREIGN KEY REFERENCES Recipes(recipeId),
-  createdAt DATETIME DEFAULT GETDATE()
-);
-
 CREATE TABLE UsersLike(
   ulId INT PRIMARY KEY IDENTITY(1, 1),
   userId INT FOREIGN KEY REFERENCES Users(userId),
@@ -135,62 +128,16 @@ WHERE r.recipeName COLLATE SQL_Latin1_General_CP1_CI_AI LIKE '%' + 'ga' COLLATE 
 ORDER BY r.recipeId;
 
 
--- UserViewRecipe
-INSERT INTO UsersViewRecipesHistory (userId, recipeId) VALUES (1, 1);
-
--- Top Trending
-SELECT TOP 10
-    r.recipeId,
-    r.recipeName,
-    r.image,
-    r.likeQuantity,
-    COUNT(uvr.recipeId) AS viewCount
-FROM Recipes r
-LEFT JOIN UsersViewRecipesHistory uvr ON r.recipeId = uvr.recipeId
-GROUP BY r.recipeId, r.recipeName, r.image, r.likeQuantity
-ORDER BY viewCount DESC;
-
--- Lấy lịch sử xem công thức
-SELECT * FROM UsersViewRecipesHistory WHERE UserId = 1
-
-
--- Lấy toàn bộ công thức
-SELECT 
-    r.recipeId,
-    r.recipeName,
-    r.image,
-    r.likeQuantity
-FROM Recipes r;
-
-SELECT 
-    cs.recipeId,
-    cs.indexStep,
-    cs.content AS stepContent
-FROM CookingSteps cs
-ORDER BY cs.recipeId, cs.indexStep;
-
-SELECT 
-    ri.recipeId,
-    i.ingredientId,
-    i.ingredientName,
-    ri.weight,
-    ri.unit
-FROM RecipesIngredients ri
-JOIN Ingredients i ON ri.ingredientId = i.ingredientId
-ORDER BY ri.recipeId;
-
 -- Kiểm tra thông tin bảng
 SELECT * FROM Users;
 SELECT * FROM Recipes;
 SELECT * FROM Ingredients;
 SELECT * FROM RecipesIngredients;
 SELECT * FROM CookingSteps;
-SELECT * FROM UsersViewRecipesHistory;
 
 DROP DATABASE ChefMateDB;
 
 DROP TABLE UsersComment;
-DROP TABLE UsersViewRecipesHistory;
 DROP TABLE UsersLike;
 DROP TABLE CookingSteps;
 DROP TABLE RecipesIngredients;
