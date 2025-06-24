9784e72533b7f1d554987549f6b9c2f96547a67c
Dương Phong <dhphong266@gmail.com>
2025-06-24 22:52:27 +0700

    add deleteComment method

--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -6,5 +6,6 @@ router.post('/like', interactionController.likeRecipe);
 router.post('/comment', interactionController.addComment);
 router.post('/increase-view-count', interactionController.increaseViewCount);
 router.get('/comments', interactionController.getAllComments);
+router.delete('/comment', interactionController.deleteComment);
 
 module.exports = router;
