e78a859094989ddb7f1bf480b9bb159ea4ffa5c6
Phong Dương <dhphong266@gmail.com>
2026-03-22 09:00:23 +0000

    feat: add session pagination, delete session and update title endpoints

--- routes/aiChatRoutes.js
+++ routes/aiChatRoutes.js
@@ -5,6 +5,8 @@ const aiChatController = require('../controllers/aiChatController');
 router.post('/sessions', aiChatController.createSession);
 router.get('/sessions', aiChatController.getSessionsByUser);
 router.get('/sessions/:sessionId', aiChatController.getSessionHistory);
+router.delete('/sessions/:id', aiChatController.deleteSession);
+router.patch('/sessions/title', aiChatController.updateSessionTitle);
 router.patch('/sessions/active-recipe', aiChatController.updateActiveRecipe);
 router.post('/recommendations-from-pantry', aiChatController.getRecommendationsFromPantry);
 router.post('/messages', aiChatController.sendMessage);
